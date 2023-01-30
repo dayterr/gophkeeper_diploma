cec8f326d7e087e9dbe9754b37df1780c8fd18b7
Mariya Dayter <mvdayter@gmail.com>
2023-01-30 03:44:44 +0300

    sending to 1st review

--- cmd/agent/envs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export ADDRESS_CERT=/Users/ruth/coding/Golang/gophkeeper_diploma/certs/ca.crt
-export ADDRESS_CERT_KEY=/Users/ruth/coding/Golang/gophkeeper_diploma/certs/ca.key
-export DATABASE_DSN=postgres://postgres:postgressheli@localhost:5432/postgres?sslmode=disable
-export JWT_KEY=aOw?FdDYIPsf6
-
-export ADDRESS_CERT=/Users/ruth/coding/Golang/gophkeeper_diploma/certs/client.crt
-export ADDRESS_CERT_KEY=/Users/ruth/coding/Golang/gophkeeper_diploma/certs/client.key
